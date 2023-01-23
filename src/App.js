7f9d4a8710f227e829e64ae467bcddccfe281f7f
perla serrano <perlaserrano2015@outlook.com>
2023-01-23 08:50:27 -0400

    split screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { SplitScreen } from "./SplitScreen";
+
+const LeftHandComponent = ( { name }) => {
+  return<h1 style={{backgroundColor:'green'}}>Left!</h1>
+}
+
+const RightHandComponent = ({ message }) =>{
+  return <p style={{backgroundColor:'red'}}>Right!</p>
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SplitScreen leftWeight={1}
+    rightWeight={3}>
+ <LeftHandComponent name="Shaun"/>
+ <RightHandComponent message="Hello"/>   
+    </SplitScreen>
+    
   );
 }
 
---
+++ src/SplitScreen.js
@@ -0,0 +1,31 @@
+import styled from "styled-components"
+import React from "react"
+
+const Container = styled.div`
+display:flex,
+`
+const Pane = styled.div`
+flex:${props => props.weight};
+`
+
+export const SplitScreen = ({
+    children,
+    leftWeight =1,
+    rightWeight= 1,
+}) =>{
+   const [left,right] = children;
+
+    return (
+        <Container>
+            <Pane weight={leftWeight}>
+                {left}
+            </Pane>
+
+            <Pane weight={rightWeight}>
+                {right}
+            </Pane>
+        </Container>
+    )
+}
+
+export default SplitScreen;
